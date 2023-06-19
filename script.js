2e83e632ed524e78ed1ab019c414f169b2391128
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-06-20 02:09:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function palindrome(str){
 	for(let i=0; i<Math.floor(str.length()/2); i++;){
-		if(str.charAt(i)!==str.charAt(str.length-i-i)){
+		if(str.charAt(i)!==str.charAt(str.length-i-1)){
 			return false;
 		}
 	}
